a3b7d8403597cb34496768134c4a582814e5550f
David Monarrez and Slater Smith <david+slater@>
2019-01-30 15:48:57 -0800

    create second button functionality and toggle button

--- js/scripts.js
+++ js/scripts.js
@@ -90,16 +90,33 @@ var playerOne = new Player(0);
 var playerTwo = new Player(0);
 
 $(document).ready(function () {
-  //var dice = getRandomInt(1, 6);
-  //playerOne = new Player(dice);
-  $('#roll').click(function () {
+  $('#roll-player-one').click(function () {
     playerOne.dice = getRandomInt(1, 6);
     playerOne.calcRoundScore()
     $('.result').text(playerOne.roundScore);
-    // console.log(playerOne.calcRoundScore());
+    $('.total-result').text(playerOne.totalScore);
     console.log(playerOne);
   });
-  // playerOne.roundScore = playerOne.calcRoundScore();
+  $('#roll-player-two').click(function () {
+    playerTwo.dice = getRandomInt(1, 6);
+    playerTwo.calcRoundScore()
+    $('.result-two').text(playerTwo.roundScore);
+    $('.total-two-result').text(playerTwo.totalScore);
+    console.log(playerTwo);
+  });
+
+
+  $('#pass-player-one').click(function () {
+    playerOne.calcTotalScore()
+    playerTwo.calcTotalScore()
+    $('.result').text(playerOne.roundScore);
+    $('.total-result').text(playerOne.totalScore);
+    $('.result-two').text(playerTwo.roundScore);
+    $('.total-two-result').text(playerTwo.totalScore);
+    $('#roll-player-one').toggle();
+    $('#roll-player-two').toggle();
+    console.log(playerOne);
+  });
 
 });
 
