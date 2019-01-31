9fadd14acdea6c0fb6a0a437c52ebdd25b37adba
David Monarrez and Slater Smith <david@>
2019-01-31 15:02:57 -0800

    create computer opponent

--- js/scripts.js
+++ js/scripts.js
@@ -34,6 +34,7 @@ var playerOne = new Player(0);
 var playerTwo = new Player(0);
 
 $(document).ready(function () {
+  // wrap in if function top once intro page is written
   $('#roll-player-one').click(function () {
     playerOne.dice = getRandomInt(1, 6);
     if (playerOne.dice === 1) {
@@ -47,7 +48,6 @@ $(document).ready(function () {
       playerOne.calcRoundScore()
       $('.result').text(playerOne.roundScore);
       $('.total-result').text(playerOne.totalScore);
-      console.log(playerOne);
     }
   });
   $('#roll-player-two').click(function () {
@@ -63,11 +63,8 @@ $(document).ready(function () {
       playerTwo.calcRoundScore()
       $('.result-two').text(playerTwo.roundScore);
       $('.total-two-result').text(playerTwo.totalScore);
-      console.log(playerTwo);
     }
   });
-
-
   $('#pass-player-one').click(function () {
     playerOne.calcTotalScore()
     playerTwo.calcTotalScore()
@@ -86,7 +83,77 @@ $(document).ready(function () {
     } else if (playerTwo.totalScore >= 100) {
       $("#winner").text("PLAYER TWO WINS")
     }
-    console.log(playerOne);
+  });
+  // wrap in else if function once intro page is written
+
+
+
+  $('#roll-human').click(function () {
+    playerOne.dice = getRandomInt(1, 6);
+    if (playerOne.dice === 1) {
+      playerOne.calcRoundScore()
+      $('.pig-right').toggle();
+      $('.pig-left').toggle();
+      $('.result').text("You rolled a 1");
+    } else {
+      playerOne.calcRoundScore()
+      $('.result').text(playerOne.roundScore);
+      $('.total-result').text(playerOne.totalScore);
+    }
+  });
+  $('#roll-player-two').click(function () {
+    playerTwo.dice = getRandomInt(1, 6);
+    if (playerTwo.dice === 1) {
+      playerTwo.calcRoundScore()
+      $('.pig-right').toggle();
+      $('.pig-left').toggle();
+      $('#roll-player-one').toggle();
+      $('#roll-player-two').toggle();
+      $('.result-two').text("You rolled a 1");
+    } else {
+      playerTwo.calcRoundScore()
+      $('.result-two').text(playerTwo.roundScore);
+      $('.total-two-result').text(playerTwo.totalScore);
+    }
+  });
+  $('#pass-computer').click(function () {
+    // playerOne.calcTotalScore()
+    // playerTwo.calcTotalScore()
+
+    if (playerOne.totalScore < 100 && playerTwo.totalScore < 100) {
+
+      for(var i = 0; i < 4; i++) {
+        // debugger;
+          playerTwo.dice = getRandomInt(1, 6);
+          if(playerTwo.dice === 1) {
+            playerTwo.roundScore = 0;
+            $("#result-"+(i+1)).text("")
+            $("#result-"+(i+2)).text("")
+            $("#result-"+(i+3)).text("")
+            $('#result-'+i).text("Robo Pig rolled a 1");
+            $('.total-two-result').text(playerTwo.totalScore);
+            break;
+          }
+          else if (i === 3 && playerTwo.dice !== 1) {
+            playerTwo.calcRoundScore()
+            $('#result-'+i).text(playerTwo.dice);
+            playerTwo.calcTotalScore()
+            $('.total-two-result').text(playerTwo.totalScore);
+          } else {
+            $('#result-'+i).text(playerTwo.dice+', ');
+            $('.total-two-result').text(playerTwo.totalScore);
+            playerTwo.roundScore += playerTwo.dice;
+            $('.pig-right').toggle();
+            $('.pig-left').toggle();
+        }
+      }
+
+    }
+    else if (playerOne.totalScore >= 100) {
+      $("#winner").text("PLAYER ONE WINS")
+    } else if (playerTwo.totalScore >= 100) {
+      $("#winner").text("ROBO PIG WINS")
+    }
   });
 
 });
